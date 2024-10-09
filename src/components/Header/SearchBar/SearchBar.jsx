727e6d000cf751a9513969f24180a9dc82adf9da
f-bravo <bravoback@outlook.com>
2024-10-09 10:40:27 -0300

    useState SearchBar

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import './SearchBar.css';
 
 function SearchBar() {
+
+  // Armazena o estado do Imput
+  const [searchValue, setSearchValue] = useState('')
   return (
     <form className='search-bar'>
       <input 
         type="search" 
         placeholder="Buscar produtos" 
+        // o valor do imput vai ser o do estado que estamos manipulando.
+        // Agora quem controla o valor desse input n é mais o HTML e sim o react
+        value={searchValue}
         className="search__input"
+        //Evento do imput onChance(): target - é o elemento que dispara o evento(o próprio input)
+        // target.value é o valor que está sendo digitado no input. É salvo dentro do estado searchValue
+        onChange={ ({ target }) => setSearchValue(target.value)}
         required
       />
+      
       <button type="submit" className="search__button">
         <BsSearch />
       </button>

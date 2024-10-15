844eb1ac0031177f1f3d47a0a835ad9e8206c74b
f-bravo <bravoback@outlook.com>
2024-10-15 11:15:23 -0300

    Components - Cart, CartItem

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Cart.css'
+import CartItem from '../CartItem/CartItem'
+
+function Cart() {
+  return (
+    <section className='cart'>
+      <div className="cart-items">
+        <CartItem data={{ thumbnail: '', title: '', price: '123'}}/>
+      </div>
+      <div className="cart-resume">Resumo do Carrinho</div>
+    </section>
+  )
+}
+
+export default Cart
